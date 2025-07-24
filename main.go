935a52f80f2fd9f27b5a199434c46076a692d9ae
Ayush Talan <ayush.talan07@gmail.com>
2025-07-24 21:00:07 +0530

    updated all comments

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"github.com/talanayush/gofr-chat/handler" 
-	"gofr.dev/pkg/gofr"                       
+	"github.com/talanayush/gofr-chat/handler"
+	"gofr.dev/pkg/gofr"
 )
 
 func main() {
 
 	app := gofr.New()
 
-
 	app.GET("/health", handler.HealthCheck)
 
 	app.GET("/swagger/*", gofr.SwaggerUIHandler)
-
 
 	app.Run()
 }
